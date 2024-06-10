5f50df9592b866bb8835a4fd9916daaae9dcb783
OTAVIO NICOLAU DA SILVA <otavio.nicollau@gmail.com>
2024-06-10 19:05:31 -0300

    Create edital_prf.js

---
+++ edital_prf.js
@@ -0,0 +1,220 @@
+
+const edital_prf =
+{
+  "EDITAL_PRF": [
+    {
+      "materia": "PORTUGUÊS",
+      "id": 101,
+      "peso": "0",
+      "nome": "1 Compreensão e interpretação de textos de gêneros variados."
+    },
+    {
+      "materia": "PORTUGUÊS",
+      "id": 102,
+      "peso": "0",
+      "nome": "2 Reconhecimento de tipos e gêneros textuais."
+    },
+    {
+      "materia": "PORTUGUÊS",
+      "id": 103,
+      "peso": "0",
+      "nome": "3 Domínio da ortografia oficial."
+    },
+    {
+      "materia": "PORTUGUÊS",
+      "id": 104,
+      "peso": "0",
+      "nome": "4 Domínio dos mecanismos de coesão textual."
+    },
+    {
+      "materia": "PORTUGUÊS",
+      "id": 105,
+      "peso": "0",
+      "nome": "4.1 Emprego de elementos de referenciação, substituição e repetição, de conectores e de outros elementos de sequenciação textual."
+    },
+    {
+      "materia": "PORTUGUÊS",
+      "id": 106,
+      "peso": "0",
+      "nome": "4.2 Emprego de tempos e modos verbais."
+    },
+    {
+      "materia": "PORTUGUÊS",
+      "id": 107,
+      "peso": "0",
+      "nome": "5 Domínio da estrutura morfossintática do período."
+    },
+    {
+      "materia": "PORTUGUÊS",
+      "id": 108,
+      "peso": "0",
+      "nome": "5.1 Emprego das classes de palavras."
+    },
+    {
+      "materia": "PORTUGUÊS",
+      "id": 109,
+      "peso": "0",
+      "nome": "5.2 Relações de coordenação entre orações e entre termos da oração."
+    },
+    {
+      "materia": "PORTUGUÊS",
+      "id": 110,
+      "peso": "0",
+      "nome": "5.3 Relações de subordinação entre orações e entre termos da oração."
+    },
+    {
+      "materia": "PORTUGUÊS",
+      "id": 111,
+      "peso": "0",
+      "nome": "5.4 Emprego dos sinais de pontuação."
+    },
+    {
+      "materia": "PORTUGUÊS",
+      "id": 112,
+      "peso": "0",
+      "nome": "5.5 Concordância verbal e nominal."
+    },
+    {
+      "materia": "PORTUGUÊS",
+      "id": 113,
+      "peso": "0",
+      "nome": "5.6 Regência verbal e nominal."
+    },
+    {
+      "materia": "PORTUGUÊS",
+      "id": 114,
+      "peso": "0",
+      "nome": "5.7 Emprego do sinal indicativo de crase."
+    },
+    {
+      "materia": "PORTUGUÊS",
+      "id": 115,
+      "peso": "0",
+      "nome": "5.8 Colocação dos pronomes átonos."
+    },
+    {
+      "materia": "PORTUGUÊS",
+      "id": 116,
+      "peso": "0",
+      "nome": "6 Reescrita de frases e parágrafos do texto."
+    },
+    {
+      "materia": "PORTUGUÊS",
+      "id": 117,
+      "peso": "0",
+      "nome": "6.1 Significação das palavras."
+    },
+    {
+      "materia": "PORTUGUÊS",
+      "id": 118,
+      "peso": "0",
+      "nome": "6.2 Substituição de palavras ou de trechos de texto."
+    },
+    {
+      "materia": "PORTUGUÊS",
+      "id": 119,
+      "peso": "0",
+      "nome": "6.3 Reorganização da estrutura de orações e de períodos do texto."
+    },
+    {
+      "materia": "PORTUGUÊS",
+      "id": 120,
+      "peso": "0",
+      "nome": "6.4 Reescrita de textos de diferentes gêneros e níveis de formalidade."
+    },
+    {
+      "materia": "PORTUGUÊS",
+      "id": 121,
+      "peso": "0",
+      "nome": "7 Correspondência oficial (conforme Manual de Redação da Presidência da República)."
+    },
+    {
+      "materia": "PORTUGUÊS",
+      "id": 122,
+      "peso": "0",
+      "nome": "7.1 Aspectos gerais da redação oficial."
+    },
+    {
+      "materia": "PORTUGUÊS",
+      "id": 123,
+      "peso": "0",
+      "nome": "7.2 Finalidade dos expedientes oficiais."
+    },
+    {
+      "materia": "PORTUGUÊS",
+      "id": 124,
+      "peso": "0",
+      "nome": "7.3 Adequação da linguagem ao tipo de documento."
+    },
+    {
+      "materia": "PORTUGUÊS",
+      "id": 125,
+      "peso": "0",
+      "nome": "7.4 Adequação do formato do texto ao gênero."
+    },
+    {
+      "materia": "RACIOCÍNIO LÓGICO-MATEMÁTICO",
+      "id": 201,
+      "peso": "0",
+      "nome": "1 Modelagem de situações-problema por meio de equações do 1º e 2º graus e sistemas lineares."
+    },
+    {
+      "materia": "RACIOCÍNIO LÓGICO-MATEMÁTICO",
+      "id": 202,
+      "peso": "0",
+      "nome": "2 Noção de função."
+    },
+    {
+      "materia": "RACIOCÍNIO LÓGICO-MATEMÁTICO",
+      "id": 203,
+      "peso": "0",
+      "nome": "2.1 Análise gráfica."
+    },
+    {
+      "materia": "RACIOCÍNIO LÓGICO-MATEMÁTICO",
+      "id": 204,
+      "peso": "0",
+      "nome": "2.2 Funções afim, quadrática, exponencial e logarítmica."
+    },
+    {
+      "materia": "RACIOCÍNIO LÓGICO-MATEMÁTICO",
+      "id": 205,
+      "peso": "0",
+      "nome": "2.3 Aplicações."
+    },
+    {
+      "materia": "RACIOCÍNIO LÓGICO-MATEMÁTICO",
+      "id": 206,
+      "peso": "0",
+      "nome": "3 Taxas de variação de grandezas."
+    },
+    {
+      "materia": "RACIOCÍNIO LÓGICO-MATEMÁTICO",
+      "id": 207,
+      "peso": "0",
+      "nome": "3.1 Razão e proporção com aplicações."
+    },
+    {
+      "materia": "RACIOCÍNIO LÓGICO-MATEMÁTICO",
+      "id": 208,
+      "peso": "0",
+      "nome": "3.2 Regra de três simples e composta."
+    },
+    {
+      "materia": "RACIOCÍNIO LÓGICO-MATEMÁTICO",
+      "id": 209,
+      "peso": "0",
+      "nome": "4 Porcentagem."
+    },
+    {
+      "materia": "RACIOCÍNIO LÓGICO-MATEMÁTICO",
+      "id": 210,
+      "peso": "0",
+      "nome": "5 Regularidades e padrões em sequências."
+    }
+  ]
+};
+
+
+
+export default edital_prf;
