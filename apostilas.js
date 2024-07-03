b31bd4ad22681a6ce2aeb819cfafff8590c64ec1
OTAVIO NICOLAU DA SILVA <otavio.nicollau@gmail.com>
2024-07-03 12:55:06 -0300

    ajustes

--- apostilas.js
+++ apostilas.js
@@ -1,15 +1,77 @@
 
-const apostilas  =
+const apostilas =
 {
   "APOSTILAS": [
     {
-      "nome": "01 - PRF-2024-MODULO-1",
+      "nome": "01 - PRF-2024-MODULO",
       "id_aula": 0,
-      "url": "https://teal-crostata-aea03c.netlify.app/public/apostilas/01PRF2024MODULO1.pdf",
+      "url": "https://teal-crostata-aea03c.netlify.app/apostilas/01-PRF-2024-MODULO-1.pdf",
+      "tipo": 'apostila',
+      "peso": "0",
+    },
+    {
+      "nome": "02 - PRF-2024-MODULO",
+      "id_aula": 0,
+      "url": "https://teal-crostata-aea03c.netlify.app/apostilas/02-PRF-2024-MODULO-2.pdf",
+      "tipo": 'apostila',
+      "peso": "0",
+    },
+    {
+      "nome": "03 - PRF-2024-JURISPRUDÊNCIAS",
+      "id_aula": 0,
+      "url": "https://teal-crostata-aea03c.netlify.app/apostilas/03-Jurisprudência-Carreiras-Policiais-Organizada-por-Assunto.pdf",
+      "tipo": 'apostila',
+      "peso": "0",
+    },
+    {
+      "nome": "04 - PRF-2024-LICITAÇÕES E CONTRATOS",
+      "id_aula": 0,
+      "url": "https://teal-crostata-aea03c.netlify.app/apostilas/04-Licitacoes-e-Contratos-Administrativos.pdf",
+      "tipo": 'apostila',
+      "peso": "0",
+    },
+    {
+      "nome": "05 - CTB",
+      "id_aula": 0,
+      "url": "https://teal-crostata-aea03c.netlify.app/apostilas/05-CTB.pdf",
+      "tipo": 'apostila',
+      "peso": "0",
+    },
+    {
+      "nome": "06 - RESOLUÇÃO 993-2023",
+      "id_aula": 0,
+      "url": "https://teal-crostata-aea03c.netlify.app/apostilas/06-R-993-de-2023.pdf",
+      "tipo": 'apostila',
+      "peso": "0",
+    },
+    {
+      "nome": "07 - RESOLUÇÃO 1.003-2023",
+      "id_aula": 0,
+      "url": "https://teal-crostata-aea03c.netlify.app/apostilas/07-R-1.003__ANEXO-fclio1.pdf",
+      "tipo": 'apostila',
+      "peso": "0",
+    },
+    {
+      "nome": "08 - RESOLUÇÃO 160-2004",
+      "id_aula": 0,
+      "url": "https://teal-crostata-aea03c.netlify.app/apostilas/08-R-160-de-2004.pdf",
+      "tipo": 'apostila',
+      "peso": "0",
+    },
+    {
+      "nome": "09 - RESOLUÇÃO 704-2017",
+      "id_aula": 0,
+      "url": "https://teal-crostata-aea03c.netlify.app/apostilas/09-R-704-de-2017.pdf",
+      "tipo": 'apostila',
+      "peso": "0",
+    },
+    {
+      "nome": "10 - RESOLUÇÃO 870-2021",
+      "id_aula": 0,
+      "url": "https://teal-crostata-aea03c.netlify.app/apostilas/10-R-870-de-2021-AnexoI.pdf",
       "tipo": 'apostila',
       "peso": "0",
     }
- 
   ]
 };
 
