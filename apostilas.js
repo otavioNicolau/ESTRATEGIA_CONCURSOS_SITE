b60e273ff34d53cefad8034905c3a33d6a8e3c26
OTAVIO NICOLAU DA SILVA <otavio.nicollau@gmail.com>
2024-07-03 12:39:44 -0300

    ajustes

--- apostilas.js
+++ apostilas.js
@@ -5,7 +5,7 @@ const apostilas  =
     {
       "nome": "01 - PRF-2024-MODULO-1",
       "id_aula": 0,
-      "url": "https://teal-crostata-aea03c.netlify.app/public/apostilas/01-PRF-2024-MODULO-1",
+      "url": "https://teal-crostata-aea03c.netlify.app/public/apostilas/01PRF2024MODULO1.pdf",
       "tipo": 'apostila',
       "peso": "0",
     }
